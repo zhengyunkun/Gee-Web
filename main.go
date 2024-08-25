f6f9b801f98dc04a0677d48b5c0085b0f75cee7f
Zheng Yunkun <2240324227@qq.com>
2024-08-25 17:08:40 +0800

    Update

--- main.go
+++ main.go
@@ -8,10 +8,13 @@ import (
 
 func main() {
 	r := gee.New()
+
+	// Add a route for GET request
 	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
 	})
 
+	// Add a route for GET request
 	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
 		for k, v := range req.Header {
 			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
