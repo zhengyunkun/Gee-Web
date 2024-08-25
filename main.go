54e5b6dfb074e981bf90e0abe7e6e94cb2881ada
Zheng Yunkun <2240324227@qq.com>
2024-08-25 17:19:28 +0800

    Update ResponseWriter and Request

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 	r := gee.New()
 
 	// Add a route for GET request
+	// ResponseWriter is used to send data to the client
+	// Request is used to get data from the client
 	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
 	})
