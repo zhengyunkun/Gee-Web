cd38e87a064c553aad2075707ac0e6dc979c1b8a
Zheng Yunkun <2240324227@qq.com>
2024-08-25 16:37:19 +0800

    Gee web frame updated

--- main.go
+++ main.go
@@ -2,27 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"gee"
 )
 
-type Engine struct {}
-
-// (engine *Engine)表示这个方法是绑定在Engine结构体上的
-func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	switch req.URL.Path {
-	case "/":
+func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-	case "/hello":
+	})
+
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
 		for k, v := range req.Header {
 			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 		}
-	default:
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
-}
+	})
 
-func main() {
-	engine := new(Engine)
-	log.Fatal(http.ListenAndServe(":9999", engine))
+	r.RUN(":8080")
 }
