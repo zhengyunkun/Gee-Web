2b9d10e0daefb9e6dbdef0ec655eaf240e52c59b
Zheng Yunkun <2240324227@qq.com>
2024-08-26 10:26:42 +0800

    Update

--- main.go
+++ main.go
@@ -26,5 +26,14 @@ func main() {
 		})
 	})
 
+	r.GET("/hello/:name", func(c *gee.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	})
+
 	r.RUN(":8080")
 }
